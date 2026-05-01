b1a33ecf11a20457b356594ef596714f05c61eb4
Matt Dolin <mgd324@lehigh.edu>
2026-04-30 20:19:00 -0400

    Updated details view to display ingredients/steps as lists

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -7,6 +7,7 @@ from django.views import generic
 from .models import Recipe, Rating
 
 import json
+import ast
 
 class IndexView(generic.ListView):
     template_name = "recipes/index.html"
@@ -17,9 +18,12 @@ class IndexView(generic.ListView):
         return Recipe.objects.order_by("-id")[:5]
 
 
-class DetailView(generic.DetailView):
-    model = Recipe
-    template_name = "recipes/detail.html"
-     
+def detail(request, recipe_id):
+    #model = Recipe
+    #template_name = "recipes/detail.html"
+    recipe = get_object_or_404(Recipe, pk = recipe_id)
+    ingredients = list(ast.literal_eval(recipe.ingredients))
+    steps = list(ast.literal_eval(recipe.steps))
+    return render(request, 'recipes/detail.html', {'recipe': recipe, 'steps': steps, 'ingredients': ingredients})
 
     
