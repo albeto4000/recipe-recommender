88db61e847faf9b6c190db6c412a4a1966c2678b
Matt Dolin <mgd324@lehigh.edu>
2026-05-13 11:29:07 -0400

    Refactored CSV load functions into one function: import_test_db

--- app/recipes/management/commands/import_test_db.py
+++ app/recipes/management/commands/import_test_db.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from django.contrib.auth.hashers import make_password
 import re
 
-from recipes.models import Recipe
+from recipes.models import Recipe, Rating
 from django.contrib.auth.models import User
 
 
@@ -18,10 +18,15 @@ class Command(BaseCommand):
 	#		parser.add_argument('csv_file', type=str)
 
 	def handle(self, *args, **options):
-		file_path = APP_DIR / 'data/recipes.csv'
-		df = pd.read_csv(file_path)
+		recipe_file_path = APP_DIR / 'data/recipes.csv'
+		recipe_df = pd.read_csv(recipe_file_path)
 
-		df_sample = df.drop_duplicates(subset = ['AuthorId']).sample(n = 100, random_state = 641).fillna(0)
+		recipe_sample = recipe_df.drop_duplicates(subset = ['AuthorId']).sample(n = 100, random_state = 641).fillna(0)
+		
+		ratings_file_path = APP_DIR / 'data/reviews.csv'
+		ratings_df = pd.read_csv(ratings_file_path)
+
+		ratings_sample = ratings_df[ratings_df['RecipeId'].isin(recipe_sample['RecipeId'])]
 
 		user_instances = [
 			User(
@@ -30,11 +35,11 @@ class Command(BaseCommand):
 				email = row['AuthorName'].lower().replace(' ', '_') + '@food.com',
 				password = make_password('1234')
 			)
-			for _, row in df_sample.iterrows()
+			for _, row in recipe_sample.iterrows()
 		]
 
-		User.objects.bulk_create(user_instances)
-		self.stdout.write(self.style.SUCCESS('Successfully imported user data'))
+		User.objects.bulk_create(user_instances, ignore_conflicts = True)
+		self.stdout.write(self.style.SUCCESS('Successfully imported recipe user data'))
 
 		recipe_instances = [
 			Recipe(
@@ -65,8 +70,35 @@ class Command(BaseCommand):
 				servings = row['RecipeServings'],
 				recipe_yield = row['RecipeYield'],
 				instructions = row['RecipeInstructions']
-			) for _, row in df_sample.iterrows()
+			) for _, row in recipe_sample.iterrows()
 		]
 
-		Recipe.objects.bulk_create(recipe_instances)
+		Recipe.objects.bulk_create(recipe_instances, ignore_conflicts = True)
 		self.stdout.write(self.style.SUCCESS('Successfully imported recipe data'))
+
+		user_instances = [
+			User(
+				pk = row['AuthorId'],
+				username = row['AuthorName'],
+				email = row['AuthorName'].lower().replace(' ', '_') + '@food.com',
+				password = make_password('1234')
+			)
+			for _, row in ratings_sample.drop_duplicates(subset = ['AuthorId']).iterrows()
+		]
+
+		User.objects.bulk_create(user_instances, ignore_conflicts = True)
+		self.stdout.write(self.style.SUCCESS('Successfully imported ratings user data'))
+
+		rating_instances = [
+			Rating(
+				pk = row['ReviewId'],
+				user = User.objects.get(pk = row['AuthorId']),
+				recipe = Recipe.objects.get(pk = row['RecipeId']),
+				rating = row['Rating'],
+				review = row['Review'],
+				date_submitted = pd.to_datetime(row['DateSubmitted'], format = 'mixed')
+			) for _, row in ratings_sample.iterrows()
+		]
+
+		Rating.objects.bulk_create(rating_instances, ignore_conflicts = True)
+		self.stdout.write(self.style.SUCCESS('Successfully imported rating data'))
