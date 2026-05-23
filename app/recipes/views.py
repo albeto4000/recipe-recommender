3317b5d588347b97ace18d11c4c37009d1b3f6c0
Matt Dolin <albeto4000@gmail.com>
2026-05-23 13:53:25 -0400

    Added comments to the other views in view.py

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -9,6 +9,8 @@ from django.core import serializers
 from django.contrib.auth import authenticate, login, get_user_model, logout
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
+from django.utils.timezone import localdate
+from django.db.models import F, Avg
 
 import re
 import json
@@ -163,15 +165,24 @@ def detail(request, recipe_id):
   })
 
 
-
+#The search view will work similarly to the browse view, but with a sidebar for the user to add filters
+#To make sure the user does not need to reload their page for their filters to apply, I'll use Ajax to get the filtered recipes, then
+#use jQuery to update the section of the page that displays those recipes. 
+#See the query route and the Javascript on search.html for that implementation
 def search(request):
+	#By default, this page will display all recipes, ordered by # of ratings descending
 	recipe_list = Recipe.objects.all().order_by('-review_count')
 
+	#Creates a paginator to display only 12 recipes at a time
 	paginator = Paginator(recipe_list, 12)
 
+	#Displays the current paginator page
 	page_number = request.GET.get('page')
 	page_obj = paginator.get_page(page_number)
 	
+	#Filters will be displayed as dropdowns with checkboxes
+	#To define the filter categories, the labels that appear, the values those labels correspond to, and the database column that
+	#the filter applies to, I'll define a list of dictionaries
 	season_filter = {
 		'category': 'season', 
 		'choices': [
@@ -182,6 +193,9 @@ def search(request):
 		'filters': 'keywords'
 	}
 
+	#Some filters, like seafood, apply one label to multiple keywords
+	#This process slows down the page rendering (as the database has to do string matching for each keyword across all
+	#500,000 recipes), so we'll look into adding new keywords on database creation
 	seafood = ['bass', 'catfish', 'crab', 'crawfish', 'fish halibut', 
 	'lobster', 'mahi mahi', 'mussels', 'no shell fish', 'octopus', 
 	'oysters', 'squid', 'tilapia', 'trout', 'tuna', 'whitefish']
@@ -193,7 +207,7 @@ def search(request):
 			('chicken', 'chicken'), 
 			('pork', 'pork|ham'), 
 			('turkey', 'turkey'),
-			('seafood', '|'.join(seafood))
+			('seafood', '|'.join(seafood)) #filters with multiple keywords combine those into one string, separated by |
 		], 
 		'filters': 'keywords'
 	}
@@ -225,55 +239,74 @@ def search(request):
 		'filters': 'keywords'
 	}
 
+	#Displays the search page with the filters defined above and the paginated recipe list
 	return render(request, 'recipes/search.html', {
 		'filters': [season_filter, protein_filter, diet_filter, cook_time_filter],
 		'page_obj': page_obj
 	})
 
 
+#The search page displays the search results without needing to reload the page
+#To accomplish this, the query route defined below 
 def query(request):
-	recipe_list = Recipe.objects.all()
-
+	#This object will be used to filter the recipes fetched from the database and displayed
 	query = Q()
 
+	#The AJAX call sends its request as JSON
+	#json.loads unpacks that data into a Python dict
 	res = json.loads(request.body)
 
+	#Users can select multiple filters, each of which may apply to one of many columns
 	for filter_col, filter_val in zip(res['filter_col'], res['filter_val']):
+		#If the user filters by keywords, split the result by | (to handle multiple keywords), then filter for rows whose keywords
+		#column contains any of the selected
 		if filter_col == 'keywords':
 			values = filter_val.split('|')
 			keyword_query = reduce(
 				or_,
 				(Q(keywords__icontains = v) for v in values)
 			)
+			#Adds the keywords filter to the query
 			query &= keyword_query
+		#FIlters by category, ignoring capitalization
 		elif filter_col == 'category':
 			query &= Q(category__in=filter_val)
+		#Filters by name, ignoring capitalization
 		elif filter_col == 'name':
 			query &= Q(name__icontains=filter_val)
 
+	#Returns any rows matching all of the filters, ordered by popularity
 	recipe_list = Recipe.objects.filter(query).order_by('-review_count')
 
+	#Chunks the resulting recipes into pages of 12
 	paginator = Paginator(recipe_list, 12)
 
+	#Fetches the 12 recipes corresponding to the current page
 	page_number = res['page']
-	print(page_number)
 	page_obj = paginator.get_page(page_number)
 
+	#Returns only the recipe display content, not the entire webpage content
 	if request.headers.get('x-requested-with') == 'XMLHttpRequest':
 		return render(request, 'recipes/paginated-recipes.html', {
 			'page_obj': page_obj,
 			'filters_selected': res['filter_label']
 	})
 
+
+#This route allows users to log in with their email
+#require_POST means a user cannot attempt to log in any other way than the login form
 @require_POST
 def login_view(request):
+	#Gets the email and password sent to the back-end (by post)
 	email = request.POST['email']
 	password = request.POST['password']
 
+	#Gets the username associated with the entered email
 	username = get_user_model().objects.get(email = email).username
-
+	#Attempts to authenticate the user given the entered username and password
 	user = authenticate(request, username = username, password = password)
 
+	#If the user credentials are valid, log in and redirect to the current webpage
 	if user is not None:
 		login(request, user)
 
@@ -283,6 +316,7 @@ def login_view(request):
 			return redirect(next_url)
 		
 		return redirect("home")
+	#If the user is not valid, send an error message, then redirect to the current webpage
 	else:
 		messages.error(request, "Invalid email or password.")
 
@@ -293,6 +327,7 @@ def login_view(request):
 		return redirect("home")
 
 
+#Logs the user out
 def logout_view(request):
     logout(request)
 
@@ -304,16 +339,21 @@ def logout_view(request):
     return redirect("home")
 
 
-from django.utils.timezone import localdate
-from django.db.models import F, Avg
-
+#Route for users to submit new recipe ratings/reviews
+#login_required means that a user who is not signed in cannot rate recipes
 @login_required
 def submit_rating(request):
+	#Gets the ID of the current recipe
 	recipe_id = int(request.POST.get('recipe_id'))
+	#Attempts to find the recipe iwth the given ID. Returns 404 error otherwise
 	recipe = get_object_or_404(Recipe, pk=recipe_id)
 
-	
-	if int(request.POST.get('rating')) > 0:
+	#rating>0 means the user has clicked a star to rate, not clicked the star of their current rating to remove it
+	if int(request.POST.get('rating')) != 0:
+		#Attempts to create a new rating, or updates the rating if it already exists
+		#To ensure that duplicate ratings are not created, I list rating, review, and date_submitted under defaults
+		#This means that if any of these values change, Django will not consider the new entry as a new rating, but instead updates
+		#the existing one corresponding to the entered user ID and recipe ID
 		new_rating, created = Rating.objects.update_or_create(
 			user=request.user,
 			recipe=recipe,
@@ -323,10 +363,12 @@ def submit_rating(request):
 				'date_submitted': localdate()
 			}
 		)	
-		
+	#If rating == 0, that means the user is attempting to remove their rating
 	else:
+		#Finds the rating corresponding to the user and recipe IDs, then deletes that rating
 		Rating.objects.get(user=request.user, recipe=recipe).delete()
 
+	#Updates the rating_count and aggregated_rating of the associated recipe
 	rating_count = Rating.objects.filter(recipe=recipe).count()
 	Recipe.objects.filter(id=recipe_id).update(review_count=rating_count)
 	if rating_count > 0:
@@ -335,4 +377,5 @@ def submit_rating(request):
 	else:
 		Recipe.objects.filter(id=recipe_id).update(aggregated_rating=0)
 
+	#Redirects to the detail page for the current recipe
 	return detail(request, recipe_id)
