3916a4d91a6457d4c731a344dbfa9e5dc92acb7f
Matt Dolin <albeto4000@gmail.com>
2026-06-03 00:15:29 -0400

    Configured app to download recommender models from Google drive, reducing file size

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+#Loads environment variables
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lrpnak(w(#@&-6myzn%+182m!xb2)5r43!n1l&hmg87!4r7p4a'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- app/recipes/apps.py
+++ app/recipes/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 from django.conf import settings
 import os
+
+import gdown
 import joblib
 
 import pandas as pd
@@ -24,8 +26,11 @@ class RecipesConfig(AppConfig):
 		from . import similar_items as similar_items
 		from .models import Recipe
 
+		if not os.path.isdir('models'):
+			gdown.download_folder('https://drive.google.com/drive/folders/17UOKeGdRGuplmA7c6B90kRQPBe_kUUWt?usp=sharing', quiet=False)
+
 		for model in self.rec_models.keys():
-			self.rec_models[model] = joblib.load(os.path.join(settings.BASE_DIR, '../models/' + model + '_pipeline.pkl'))
+			self.rec_models[model] = joblib.load(os.path.join(settings.BASE_DIR, 'models/' + model + '_pipeline.pkl'))
 
 		self.df = pd.DataFrame.from_records(Recipe.objects.all().values('id', 'category', 'keywords'))
 		_, self.tfidf_matrix = similar_items.fit_tfidf(self.df)
